e493359a0cf9334aced2538b492b69f3f125e24b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 15:35:53 +0200

    ptit cachotier cet apostrophe

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -5,10 +5,10 @@ $nbrRicard = 0;
 do {
     echo "Come on ricard number $nbrRicard\n";
     if ($nbrRicard == 3) {
-        echo "I'll have to stop soon!'\n";
+        echo "I'll have to stop soon'\n";
     }
     if ($nbrRicard == 5) {
-        echo "I am no longer very fresh...'\n";
+        echo "I am no longer very fresh...\n";
     }
     $nbrRicard++;
 } while ($nbrRicard <= 7);
