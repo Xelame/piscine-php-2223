597cb25636bb90be4ce1c334e68f4abd41139162
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 14:14:04 +0200

    ex 1 etape 1

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+$firstStr = "Hello";
+$firstEscapeStr = "\nWorld";  
+$firstBool = true;
+$firstInteger = 123;
+$firstFloat = 1.23;
+$firstNull = NULL;
+$firstArray = array(2, null, 'Hi', 5.5, false);  
+?>
