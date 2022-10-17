3606e93fc8cb82671f398f43dcc9429da94a47cf
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 16:00:37 +0200

    Etape 2 ex 3

---
+++ for-numbers.php
@@ -0,0 +1,12 @@
+<?php
+for ($number = 1; $number <= 100; $number++) {
+    $numberOfDiv = 0;
+    for ($div = 1; $div <= round(sqrt($number)); $div++) {
+        if ($number % $div == 0) {
+            $numberOfDiv++;
+        }   
+    }
+    if ($numberOfDiv == 1) {
+        echo "$number, ";
+    }
+}
