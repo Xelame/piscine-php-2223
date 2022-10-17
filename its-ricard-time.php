47afd2c2be57c50a216d2ee69b423f31dcd79ff9
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 15:46:23 +0200

    merci adrien le bg supra ultra

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -8,7 +8,7 @@ do {
         echo "I'll have to stop soon!\n";
     }
     if ($nbrRicard == 5) {
-        echo "I am no longer very fresh...\n";
+        echo "I am no longer very fresh ...\n";
     }
     if ($nbrRicard == 7) {
         echo "Come on ricard number $nbrRicard";
