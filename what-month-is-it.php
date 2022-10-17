b04ef939bf9931ad5378304419a2620b07c866ee
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 16:19:15 +0200

    test nul

--- what-month-is-it.php
+++ what-month-is-it.php
@@ -1,43 +1,7 @@
 <?php
 $month = date('F');
 
-echo "We are in the month of ";
-
 switch ($month) {
-    case 'January':
-        echo "January";
-        break;
-    case 'February':
-        echo "February";
-        break;
-    case 'March':
-        echo "March";
-        break;
-    case 'April':
-        echo "April";
-        break;
-    case 'May':
-        echo "May";
-        break;
-    case 'June':
-        echo "June";
-        break;
-    case 'July':
-        echo "July";
-        break;
-    case 'August':
-        echo "August";
-        break;
-    case 'September':
-        echo "September";
-        break;
-    case 'October':
-        echo "October";
-        break;
-    case 'November':
-        echo "November";
-        break;
-    case 'December':
-        echo "December";
-        break;
+    default:
+    echo "We are in the month of $month";
 }
