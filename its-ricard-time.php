de042c47e5e22edac32fde632185dbed990ab1d4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-17 15:39:13 +0200

    bon bah flop

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -1,11 +1,11 @@
 <?php
 
-$nbrRicard = 0;
+$nbrRicard = 1;
 
 do {
     echo "Come on ricard number $nbrRicard\n";
     if ($nbrRicard == 3) {
-        echo "I'll have to stop soon'\n";
+        echo "I'll have to stop soon!\n";
     }
     if ($nbrRicard == 5) {
         echo "I am no longer very fresh...\n";
