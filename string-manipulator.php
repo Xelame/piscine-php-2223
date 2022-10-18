45a7e75292ba7c77b35730bc3909d47cbae39629
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-18 14:50:23 +0200

    Etape 3 ex2

---
+++ string-manipulator.php
@@ -0,0 +1,21 @@
+<?php
+
+function capseMe(string $string) : string {
+    return strtoupper($string);
+}
+
+function lowerMe(string $string) : string {
+    return strtolower($string);
+}
+
+function upperCaseFirst(string $string) : string {
+    return ucfirst($string);
+}
+
+function lowerCaseFirst(string $string) : string {
+    return lcfirst($string);
+}
+
+function removeBlankSpace(string $string) : string {
+    return str_replace(' ', '', $string);
+}
