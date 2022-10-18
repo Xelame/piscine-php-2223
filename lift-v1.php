75feaca337f268b08b16d2373f9f0d9ff2dd8a31
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-18 16:26:52 +0200

    return something

--- lift-v1.php
+++ lift-v1.php
@@ -1,7 +1,24 @@
 <?php
 
 function getFloor(int $currentfloor, int|null $floorRequest, array $buttonList) : int|null {
-    return $floorRequest === null ? null : $currentfloor;
+    if ($floorRequest === null) {
+        return null;
+    }
+    if ($currentfloor === $floorRequest) {
+        return null;
+    }
+    if ($currentfloor < $floorRequest) {
+        if (in_array($currentfloor, $buttonList)) {
+            return $currentfloor;
+        }
+        return $floorRequest;
+    }
+    if ($currentfloor > $floorRequest) {
+        if (in_array($currentfloor, $buttonList)) {
+            return $currentfloor;
+        }
+        return $floorRequest;
+    }
 }
 
 function getDirection(int $currentfloor, int|null $floorRequest, array $buttonList) : int {
