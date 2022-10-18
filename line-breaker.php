2fd570f69e3bbf20c1f53ee36bb622479a051b47
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-18 15:27:13 +0200

    another trim

--- line-breaker.php
+++ line-breaker.php
@@ -5,12 +5,12 @@ function breakLines(string $string, int $lenght): string {
     $currentLine = "";
     foreach ($words as $word) {
         if (strlen($currentLine) + strlen($word) > $lenght) {
-            $currentLine = trim($currentLine);
+            
             $lines[] = $currentLine;
             $currentLine = "";
         }
         $currentLine .= $word . " ";
     }
     $lines[] = $currentLine;
-    return implode("\n", $lines);
+    return trim(implode("\n", $lines));
 }
