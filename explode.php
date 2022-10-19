9c41aa131d684421e2d62621741001e72f830c19
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:17:31 +0200

    aleed it's gut

--- explode.php
+++ explode.php
@@ -3,28 +3,23 @@
 function explodeWords(string $string, string $delimiter = ' ', int $limit = PHP_INT_MAX) : array {
     $result = [];
     $word = '';
+    $oldLimit = $limit;
+    if ($limit <= 0) {
+        $limit = PHP_INT_MAX;
+    }
     $i = 0;
-    if ($limit >= 0) {
-        while ($i < strlen($string) && count($result) < $limit - 1) {
-            if ($string[$i] === $delimiter) {
-                $result[] = $word;
-                $word = '';
-            } else {
-                $word .= $string[$i];
-            }
-            $i++;
+    while ($i < strlen($string)) {
+        if ($string[$i] === $delimiter && count($result) < $limit - 1) {
+            $result[] = $word;
+            $word = '';
+        } else {
+            $word .= $string[$i];
         }
-        $result[] = substr($string, $i);
-    } else {
-        while ($i < strlen($string)) {
-            if ($string[$i] === $delimiter) {
-                $result[] = $word;
-                $word = '';
-            } else {
-                $word .= $string[$i];
-            }
-            $i++;
-        }
+        $i++;
+    }
+    $result[] = $word;
+    if ($oldLimit < 0) {
+        $result = array_slice($result, 0, $oldLimit);
     }
     return $result;
 }
