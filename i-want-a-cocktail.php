b527508865189ca156e28d87694eb49612087e58
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 16:12:06 +0200

    aled

--- i-want-a-cocktail.php
+++ i-want-a-cocktail.php
@@ -5,4 +5,10 @@ class Mojito
     public float $alcoholRate = 0.05;
     public array $ingredients = ["rum", "lime", "sparkling", "water", "mint", "sugar"];
     public int $price = 8;
+
+    public function __construct(int $price, float $alcoholRate, array $ingredients) {
+        $this->price = $price;
+        $this->alcoholRate = $alcoholRate;
+        $this->ingredients = $ingredients;
+    }
 }
