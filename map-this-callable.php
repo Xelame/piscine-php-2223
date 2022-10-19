dc1c525e36cffea979f3db07c30e62305a5f64c2
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 11:31:49 +0200

    test ...

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,5 +1,16 @@
 <?php
-function myArrayMap(callable $callback, array $array, array ...$arrays) {
+function myArrayMap(?callable $callback, array $array, array ...$arrays) {
+    if ($callback === null) {
+        $newArray = [];
+        for ($i = 0; $i < count($array); $i++) {
+            $content = [$array[$i]];
+            foreach ($arrays as $array) {
+                $content[] = $array[$i];
+            }
+            $newArray[] = $content;
+        }
+        return $newArray;
+    }
     $result = [];
     foreach ($array as $key => $value) {
         $result[$key] = $callback($value);
