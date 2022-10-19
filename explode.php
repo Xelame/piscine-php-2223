f7017cf1265020bb992893b365ac366ae0f3b052
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:03:13 +0200

    test

--- explode.php
+++ explode.php
@@ -4,15 +4,30 @@ function explodeWords(string $string, string $delimiter = ' ', int $limit = PHP_
     $result = [];
     $word = '';
     $i = 0;
-    while ($i < strlen($string)) {
-        if ($string[$i] === $delimiter && count($result) < $limit - 1) {
-            $result[] = $word;
-            $word = '';
-        } else {
-            $word .= $string[$i];
+    if ($limit >= 0) {
+        while ($i < strlen($string) && count($result) < $limit - 1) {
+            if ($string[$i] === $delimiter) {
+                $result[] = $word;
+                $word = '';
+            } else {
+                $word .= $string[$i];
+            }
+            $i++;
         }
-        $i++;
+        $result[] = substr($string, $i);
+    } else {
+        while ($i < strlen($string)) {
+            if ($string[$i] === $delimiter) {
+                $result[] = $word;
+                $word = '';
+            } else {
+                $word .= $string[$i];
+            }
+            $i++;
+        }
     }
-    $result[] = $word;
     return $result;
 }
+
+echo implode(' ', explodeWords('La-fonction/explode/est/trop-bien', '/', 2));
+echo implode(' ', explodeWords('La-fonction/explode/est/trop-bien', '/', -1));
