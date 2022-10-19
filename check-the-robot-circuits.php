699a409f1dd8f2488e648d5e73d57c53d8b4c806
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:59:57 +0200

    remove else

--- check-the-robot-circuits.php
+++ check-the-robot-circuits.php
@@ -4,15 +4,20 @@ function checkCircuits(int $circuit) : array {
     $debugger = [];
     if ($circuit%2 == 0) {
         $debugger[] = "Left arm";
-    } elseif ($circuit%3 == 0) {
+    } 
+    if ($circuit%3 == 0) {
         $debugger[] = "Right arm";
-    } elseif ($circuit%5 == 0) {
+    } 
+    if ($circuit%5 == 0) {
         $debugger[] = "Motherboard";
-    } elseif ($circuit%7 == 0) {
+    } 
+    if ($circuit%7 == 0) {
         $debugger[] = "Processor";
-    } elseif ($circuit%11 == 0) {
+    } 
+    if ($circuit%11 == 0) {
         $debugger[] = "Zip Defluxer";
-    } elseif ($circuit%13 == 0) {
+    } 
+    if ($circuit%13 == 0) {
         $debugger[] = "Moter";
     }
     return $debugger;
