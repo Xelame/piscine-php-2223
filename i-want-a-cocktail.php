be6939598f222f69de5903a355d64092c90a803b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 20:31:50 +0200

    aled

--- i-want-a-cocktail.php
+++ i-want-a-cocktail.php
@@ -9,13 +9,6 @@ class Mojito
     public function __construct() {
         $this->price = 8;
         $this->alcoholRate = 0.15;
-        $this->ingredients = 
-            [
-                'lime' => 1,
-                'sugar' => 1,
-                'mint' => 1,
-                'rum' => 1,
-                'soda' => 1
-            ];
+        $this->ingredients = ['rum', 'lime', 'sparkling water', 'mint', 'sugar'];
     }
 }
