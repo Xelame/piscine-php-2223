f6532bac7bfca9fb3775cd958adfafa9ac360108
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:30:23 +0200

    aled

--- manage-robot-movements.php
+++ manage-robot-movements.php
@@ -4,14 +4,14 @@ function manageMovements(string $instruction) : array {
     $movements = [];
     $previousMove = '';
     foreach (str_split($instruction) as $move) {
-        if ($move == "R") {
-            $movements[] = "RIGHT" . $previousMove != $move ? "" : " AGAIN";
-        } elseif ($move == "L") {
-            $movements[] = "LEFT" . $previousMove != $move ? "" : " AGAIN";
-        } elseif ($move == "F") {
-            $movements[] = "FRONT" . $previousMove != $move ? "" : " AGAIN";
-        } elseif ($move == "B") {
-            $movements[] = "BACKWARDS" . $previousMove != $move ? "" : " AGAIN";
+        if ($move == 'R') {
+            $movements[] = 'RIGHT' . $previousMove != $move ? '' : ' AGAIN';
+        } elseif ($move == 'L') {
+            $movements[] = 'LEFT' . $previousMove != $move ? '' : ' AGAIN';
+        } elseif ($move == 'F') {
+            $movements[] = 'FRONT' . $previousMove != $move ? '' : ' AGAIN';
+        } elseif ($move == 'B') {
+            $movements[] = 'BACKWARDS' . $previousMove != $move ? '' : ' AGAIN';
         }
         $previousMove = $move;
     }
