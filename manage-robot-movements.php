dee4af2ba5f78a79f317b788ad7fc9318e4152ba
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:26:06 +0200

    Etape 4 ex 4

---
+++ manage-robot-movements.php
@@ -0,0 +1,18 @@
+<?php
+
+function manageMovements(string $instruction) : array {
+    $movements = [];
+    $previousMove = '';
+    foreach (str_split($instruction) as $move) {
+        if ($move == "R") {
+            $movements[] = "RIGHT" . $previousMove != $move ? "" : " AGAIN";
+        } elseif ($move == "L") {
+            $movements[] = "LEFT" . $previousMove != $move ? "" : " AGAIN";
+        } elseif ($move == "F") {
+            $movements[] = "FRONT" . $previousMove != $move ? "" : " AGAIN";
+        } elseif ($move == "B") {
+            $movements[] = "BACKWARDS" . $previousMove != $move ? "" : " AGAIN";
+        }
+        $previousMove = $move;
+    }
+}
