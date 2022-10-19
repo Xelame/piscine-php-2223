8987c0aa82d1380b140f69c29ebf60ca3beb0923
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-19 03:55:19 +0200

    Etape 4 ex 5

---
+++ check-the-robot-circuits.php
@@ -0,0 +1,18 @@
+<?php
+
+function checkCircuits(int $circuit) : array {
+    $debugger = [];
+    if ($circuit%2 == 0) {
+        $debugger[] = "Left arm";
+    } elseif ($circuit%3 == 0) {
+        $debugger[] = "Right arm";
+    } elseif ($circuit%5 == 0) {
+        $debugger[] = "Motherboard";
+    } elseif ($circuit%7 == 0) {
+        $debugger[] = "Processor";
+    } elseif ($circuit%11 == 0) {
+        $debugger[] = "Zip Defluxer";
+    } elseif ($circuit%13 == 0) {
+        $debugger[] = "Moter";
+    }
+}
